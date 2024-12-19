4679784e4fcd8f0e83a45cf0f92ad1222ee72314
tri dang <tri.dd@gianty.com.vn>
2024-12-19 23:26:07 +0700

    entry fx

--- Source/Warrior/Private/AbilitySystem/Tasks/AbilityTask_WaitSpawnEnemies.cpp
+++ Source/Warrior/Private/AbilitySystem/Tasks/AbilityTask_WaitSpawnEnemies.cpp
@@ -4,10 +4,13 @@
 #include "AbilitySystem/Tasks/AbilityTask_WaitSpawnEnemies.h"
 
 #include "AbilitySystemComponent.h"
+#include "NavigationSystem.h"
+#include "Character/WarriorEnemyCharacter.h"
+#include "Engine/AssetManager.h"
 
 UAbilityTask_WaitSpawnEnemies* UAbilityTask_WaitSpawnEnemies::WaitSpawnEnemies(UGameplayAbility* OwningAbility,
                                                                                FGameplayTag EventTag, TSoftClassPtr<AWarriorEnemyCharacter> SoftEnemyClassToSpawn, int32 NumToSpawn,
-                                                                               const FVector& SpawnOrigin, float RandomSpawnRadius, const FRotator& SpawnRotation)
+                                                                               const FVector& SpawnOrigin, float RandomSpawnRadius)
 {
 	UAbilityTask_WaitSpawnEnemies* Node = NewAbilityTask<UAbilityTask_WaitSpawnEnemies>(OwningAbility);
 	Node->CachedEventTag = EventTag;
@@ -15,7 +18,6 @@ UAbilityTask_WaitSpawnEnemies* UAbilityTask_WaitSpawnEnemies::WaitSpawnEnemies(U
 	Node->CachedNumToSpawn = NumToSpawn;
 	Node->CachedSpawnOrigin = SpawnOrigin;
 	Node->CachedRandomSpawnRadius = RandomSpawnRadius;
-	Node->CachedSpawnRotation = SpawnRotation;
 	
 	return Node;
 }
@@ -36,6 +38,69 @@ void UAbilityTask_WaitSpawnEnemies::OnDestroy(bool bInOwnerFinished)
 
 void UAbilityTask_WaitSpawnEnemies::OnGameplayEventReceived(const FGameplayEventData* Payload)
 {
-	UE_LOG(LogTemp, Display, TEXT("OnGameplayEventReceived"));
+	if (ensure(!CachedSoftEnemyClassToSpawn.IsNull()))
+	{
+		UAssetManager::Get().GetStreamableManager().RequestAsyncLoad
+		(
+			CachedSoftEnemyClassToSpawn.ToSoftObjectPath(),
+		FStreamableDelegate::CreateUObject(this, &ThisClass::OnEnemyClassLoaded)
+		);
+	}
+	else
+	{
+		if (ShouldBroadcastAbilityTaskDelegates())
+		{
+			DidNotSpawn.Broadcast(TArray<AWarriorEnemyCharacter*>());
+		}
+		EndTask();
+	}
+	
+}
+
+void UAbilityTask_WaitSpawnEnemies::OnEnemyClassLoaded()
+{
+	UClass* LoadedClass = CachedSoftEnemyClassToSpawn.Get();
+	UWorld* World = GetWorld();
+
+	FActorSpawnParameters  SpawnParam;
+	SpawnParam.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
+
+	
+	if (!LoadedClass || !World)
+	{
+		if (ShouldBroadcastAbilityTaskDelegates())
+		{
+			DidNotSpawn.Broadcast(TArray<AWarriorEnemyCharacter*>());
+		}
+		EndTask();
+		return;
+	}
+	TArray<AWarriorEnemyCharacter*> SpawnedEnemies;
+	for (int32 i = 0; i < CachedNumToSpawn; i++)
+	{
+		FVector RandomLocation;
+		UNavigationSystemV1::K2_GetRandomLocationInNavigableRadius(this, CachedSpawnOrigin, RandomLocation, CachedRandomSpawnRadius);
+		RandomLocation += FVector(0.f, 0.f, 150.f);
+
+		const FRotator SpawnedRotation = AbilitySystemComponent->GetAvatarActor()->GetActorForwardVector().ToOrientationRotator();
+		
+		AWarriorEnemyCharacter* SpawnedEnemy = World->SpawnActor<AWarriorEnemyCharacter>(LoadedClass, RandomLocation, SpawnedRotation,SpawnParam);
+
+		if (SpawnedEnemy)
+		{
+			SpawnedEnemies.AddUnique(SpawnedEnemy);
+		}
+	}
+	if (ShouldBroadcastAbilityTaskDelegates())
+	{
+		if (!SpawnedEnemies.IsEmpty())
+		{
+			OnSpawnFinished.Broadcast(SpawnedEnemies);
+		}
+		else
+		{
+			DidNotSpawn.Broadcast(TArray<AWarriorEnemyCharacter*>());
+		}
+	}
 	EndTask();
 }
--- Source/Warrior/Public/AbilitySystem/Tasks/AbilityTask_WaitSpawnEnemies.h
+++ Source/Warrior/Public/AbilitySystem/Tasks/AbilityTask_WaitSpawnEnemies.h
@@ -24,8 +24,7 @@ public:
 		TSoftClassPtr<AWarriorEnemyCharacter> SoftEnemyClassToSpawn,
 		int32 NumToSpawn,
 		const FVector& SpawnOrigin,
-		float RandomSpawnRadius,
-		const FRotator& SpawnRotation);
+		float RandomSpawnRadius);
 
 	UPROPERTY(BlueprintAssignable)
 	FWaitSpawnEnemiesDelegate OnSpawnFinished;
@@ -44,12 +43,13 @@ private:
 	int32 CachedNumToSpawn;
 	FVector CachedSpawnOrigin;
 	float CachedRandomSpawnRadius;
-	FRotator CachedSpawnRotation;
+	
+
+	FDelegateHandle DelegateHandle;
 
 	void OnGameplayEventReceived(const FGameplayEventData* Payload);
-	/**
-	 * 
-	 */
-	FDelegateHandle DelegateHandle;
+	void OnEnemyClassLoaded();
+
+	
 };
 
